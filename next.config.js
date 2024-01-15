47fc8456a3f5e04cc3715110aa4d1dd2365a75b1
Mike Kiruti <Kiruti01>
2024-01-15 23:55:47 +0300

    again

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Enable experimental features
   experimental: {
     appDir: true,
+    reactRoot: true, // Enables React 18 concurrent features (when available)
+    serverComponents: true, // Enables experimental server components (when available)
+    optimizeCss: true, // Enables experimental CSS optimizations
+    // ... other experimental features
   },
-}
 
-module.exports = nextConfig
+  // Webpack configurations
+  webpack: (config, options) => {
+    // Your webpack configurations go here
+    return config;
+  },
+};
+
+module.exports = nextConfig;
